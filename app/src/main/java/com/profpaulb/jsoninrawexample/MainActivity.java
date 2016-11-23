9b0140610cf5bceb48de758205dc9fcacb04f3f3
Paul Bonenfant <Paul.Bonenfant@gmail.com>
2016-11-23 12:45:31 -0500

    Initial commit

---
+++ app/src/main/java/com/profpaulb/jsoninrawexample/MainActivity.java
@@ -0,0 +1,31 @@
+package com.profpaulb.jsoninrawexample;
+
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void loadGrades(View view) {
+
+        // Fun starts here!
+
+    }
+
+
+}
